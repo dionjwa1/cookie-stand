ea6be5d00334cc9e72a252de855feec2e02d8194
Dion Johnson <dionjwa@gmail.com>
2021-01-26 22:05:58 -0800

    updated js variables

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
-console.log('Yo');
+//console.log('Yo');
+let myContainer = document.getElementById('container')
 
 let hours =['6am', '7am', '8am', '9am', '10am','11am','12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
@@ -9,4 +10,9 @@ let seattle = {
   min:23,
   max:65,
   avg:6.3,
+
 }
+
+let ul = document.createElement('ul');
+ul.
+
