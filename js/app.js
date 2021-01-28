8ba567b1a13e84ab65944680c0489461f52c9fce
Dion Johnson <dionjwa@gmail.com>
2021-01-27 22:33:26 -0800

    adjusted DOM

--- js/app.js
+++ js/app.js
@@ -1,26 +1,25 @@
 'use strict';
 
 //console.log('Yo');
-// let myContainer = document.getElementById('container');
 
 const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 let seattle = {
-  name:'Seattle',
-  minCustomerEachHour:23,
-  maxCustomerEachHour:65,
-  avgCookieSoldPerCustomer:6.3,
+  name: 'Seattle',
+  minCustomerEachHour: 23,
+  maxCustomerEachHour: 65,
+  avgCookieSoldPerCustomer: 6.3,
   cookiesSoldPerCustomerArry: [],
-  dailyStoreTotal:0,
-  randomCustomerEachHour: function() {
+  dailyStoreTotal: 0,
+  randomCustomerEachHour: function () {
     console.log('at randomCustomerEachHour works');
-    return Math.floor(Math.random() * (this.maxCustomerEachHour - this.minCustomerEachHour + 1) + this.minCustomerEachHour);
+    return Math.round(Math.random() * (this.maxCustomerEachHour - this.minCustomerEachHour + 1) + this.minCustomerEachHour);
   },
   //method to cal and populate # of cookies sold per hour
   calcCookiesSoldEachHour: function () {
-    let randomCookiesSoldPerHour = this.randomCustomerEachHour();
-    console.log(randomCookiesSoldPerHour);
+    // console.log(randomCookiesSoldPerHour);
     for (let i = 0; i < hours.length; i++) {
+      let randomCookiesSoldPerHour = this.randomCustomerEachHour();
       this.cookiesSoldPerCustomerArry.push(randomCookiesSoldPerHour * this.avgCookieSoldPerCustomer);
     }
 
@@ -29,9 +28,23 @@ let seattle = {
   render: function () {
     this.calcCookiesSoldEachHour();
     console.log('I am in the render method');
+    let myContainer = document.getElementById('container');
+    console.log(myContainer);
+    let div= document.createElement('h2');
+    let ul = document.createElement('ul');
+    for (let i = 0; i < hours.length; i++) {
+      let li = document.createElement('li');
+      let h2 = document.createElement('h2');
+      li.innerHTML = hours[i] + ' ' + this.cookiesSoldPerCustomerArry[i];
+      h2.innerHTML = `Seattle`;
+      ul.append(li);
+      div.append(h2);
+
+    }
+    myContainer.append(ul);
   },
 };
-
+console.log(seattle.cookiesSoldPerCustomerArry);
 let cookieCount = [hours[0] + seattle.cookiesSoldPerCustomerArry[0]];
 console.log(cookieCount);
 
