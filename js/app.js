bb00c33a4790e563f1f0521cbaa332f59f9bcc4e
Dion Johnson <dionjwa@gmail.com>
2021-01-27 21:59:09 -0800

    updated salmon cookies js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,139 @@
 'use strict';
 
 //console.log('Yo');
-let myContainer = document.getElementById('container')
+// let myContainer = document.getElementById('container');
 
-let hours =['6am', '7am', '8am', '9am', '10am','11am','12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+const hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 let seattle = {
-  name: 'Seattle',
-  min:23,
-  max:65,
-  avg:6.3,
+  name:'Seattle',
+  minCustomerEachHour:23,
+  maxCustomerEachHour:65,
+  avgCookieSoldPerCustomer:6.3,
+  cookiesSoldPerCustomerArry: [],
+  dailyStoreTotal:0,
+  randomCustomerEachHour: function() {
+    console.log('at randomCustomerEachHour works');
+    return Math.floor(Math.random() * (this.maxCustomerEachHour - this.minCustomerEachHour + 1) + this.minCustomerEachHour);
+  },
+  //method to cal and populate # of cookies sold per hour
+  calcCookiesSoldEachHour: function () {
+    let randomCookiesSoldPerHour = this.randomCustomerEachHour();
+    console.log(randomCookiesSoldPerHour);
+    for (let i = 0; i < hours.length; i++) {
+      this.cookiesSoldPerCustomerArry.push(randomCookiesSoldPerHour * this.avgCookieSoldPerCustomer);
+    }
 
-}
+  },
 
-let ul = document.createElement('ul');
-ul.
+  render: function () {
+    this.calcCookiesSoldEachHour();
+    console.log('I am in the render method');
+  },
+};
+
+let cookieCount = [hours[0] + seattle.cookiesSoldPerCustomerArry[0]];
+console.log(cookieCount);
+
+
+
+
+
+// let tokyo = {
+//   name: 'Tokyo',
+//   minCustomerEachHour: 23,
+//   maxCustomerEachHour: 65,
+//   avgCookieSoldPerCustomer: 6.3,
+//   cookiesSoldPerCustomer: [],
+//   dailyStoreTotal: 0,
+//   randomCustomerEachHour: function () {
+//     console.log('at randomCustomerEachHour works');
+//     return Math.floor(Math.random() * (this.maxCustomerEachHour - this.minCustomerEachHour) + this.minCustomerEachHour);
+//   },
+//   calcCookiesSoldEachHour: function () {
+//     let randomCustomerForOneHour = this.randomCustomerEachHour();
+//     console.log(randomCustomerForOneHour);
+//   },
+
+//   render: function () {
+//     this.calcCookiesSoldEachHour();
+//     console.log('I am in the render method');
+//   }
+// };
+
+// let dubai = {
+//   name: 'Dubai',
+//   minCustomerEachHour: 23,
+//   maxCustomerEachHour: 65,
+//   avgCookieSoldPerCustomer: 6.3,
+//   cookiesSoldPerCustomer: [],
+//   dailyStoreTotal: 0,
+//   randomCustomerEachHour: function () {
+//     console.log('at randomCustomerEachHour works');
+//     return Math.floor(Math.random() * (this.maxCustomerEachHour - this.minCustomerEachHour) + this.minCustomerEachHour);
+//   },
+//   calcCookiesSoldEachHour: function () {
+//     let randomCustomerForOneHour = this.randomCustomerEachHour();
+//     console.log(randomCustomerForOneHour);
+//   },
+
+//   render: function () {
+//     this.calcCookiesSoldEachHour();
+//     console.log('I am in the render method');
+//   }
+// };
+
+// let paris = {
+//   name: 'Paris',
+//   minCustomerEachHour: 23,
+//   maxCustomerEachHour: 65,
+//   avgCookieSoldPerCustomer: 6.3,
+//   cookiesSoldPerCustomer: [],
+//   dailyStoreTotal: 0,
+//   randomCustomerEachHour: function () {
+//     console.log('at randomCustomerEachHour works');
+//     return Math.floor(Math.random() * (this.maxCustomerEachHour - this.minCustomerEachHour) + this.minCustomerEachHour);
+//   },
+//   calcCookiesSoldEachHour: function () {
+//     let randomCustomerForOneHour = this.randomCustomerEachHour();
+//     console.log(randomCustomerForOneHour);
+//   },
+
+//   render: function () {
+//     this.calcCookiesSoldEachHour();
+//     console.log('I am in the render method');
+//   }
+// };
+
+// let lima = {
+//   name: 'Lima',
+//   minCustomerEachHour: 23,
+//   maxCustomerEachHour: 65,
+//   avgCookieSoldPerCustomer: 6.3,
+//   cookiesSoldPerCustomer: [],
+//   dailyStoreTotal: 0,
+//   randomCustomerEachHour: function () {
+//     console.log('at randomCustomerEachHour works');
+//     return Math.floor(Math.random() * (this.maxCustomerEachHour - this.minCustomerEachHour) + this.minCustomerEachHour);
+//   },
+//   calcCookiesSoldEachHour: function () {
+//     let randomCustomerForOneHour = this.randomCustomerEachHour();
+//     console.log(randomCustomerForOneHour);
+//   },
+
+//   render: function () {
+//     this.calcCookiesSoldEachHour();
+//     console.log('I am in the render method');
+//   }
+// };
+
+seattle.render();
+// tokyo.render();
+// dubai.render();
+// paris.render();
+// lima.render();
+
+
+// let ul = document.createElement('ul');
+// <li id="seattle"
 
